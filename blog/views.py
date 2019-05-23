de8b90308a9129bdbfc208f3b45b21d8075c1064
Adriano <adriano.ramos408@gmail.com>
2019-05-22 21:33:12 -0300

    Site que era pra funfar e nao funfou

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+def postagem(request):
+    return render(request, 'blog/postagem.html', {})
