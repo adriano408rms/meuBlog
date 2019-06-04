d729c573c7b9b32a1034da547d01d7075e4b1703
Adriano <adriano.ramos408@gmail.com>
2019-06-03 21:28:11 -0300

    Criando o formulario

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('postagem', views.postagem, name='postagem'),
+    path('postagem/', views.postagem, name='postagem'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
 
 ]
